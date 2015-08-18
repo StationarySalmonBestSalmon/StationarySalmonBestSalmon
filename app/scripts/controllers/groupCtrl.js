c0367b16a15d583ab37b22a7627335cb542ce9fa
Mychael Zuniga <mychael.zuniga@yahoo.com>
2015-08-17 17:06:03 -0700

    style and remove old services

--- app/scripts/controllers/groupCtrl.js
+++ app/scripts/controllers/groupCtrl.js
@@ -1,5 +1,5 @@
-app.
-  controller('groupCtrl', ['$scope', '$stateParams', '$cookies', '$window', 'ElectEventAPI',
+app
+  .controller('groupCtrl', ['$scope', '$stateParams', '$cookies', '$window', 'ElectEventAPI',
     function ($scope, $stateParams, $cookies, $window, ElectEventAPI) {
       var contains = function (array, property, target) {
         for (var i = 0, arrayLength = array.length; i < arrayLength; i++) {
--- app/scripts/controllers/groupSearch.js
+++ app/scripts/controllers/groupSearch.js
@@ -1,5 +1,5 @@
-app.
-  controller('groupSearch', ['$scope', 'ElectEventAPI', function ($scope, ElectEventAPI) {
+app
+  .controller('groupSearch', ['$scope', 'ElectEventAPI', function ($scope, ElectEventAPI) {
     ElectEventAPI.getAllGroups().then(function (response) {
       $scope.groups = response.data;
     });
